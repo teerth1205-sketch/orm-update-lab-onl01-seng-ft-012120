f2d8ab388716d4c9c5d94dc0af262da51d5404a1
Teerth Patel <teerth1205@gmail.com>
2020-02-23 01:21:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,8 +52,10 @@ def self.new_from_db(row)
  def self.find_by_name(names)
    sql = "SELECT * FROM students WHERE name = ?"
    
+  DB[:conn].execute(sql, names).map |row|
+  Student.new_from_db(row)
+  end.first 
   
-   Student.new_from_db(DB[:conn].execute(sql, names))
    
 end     
   
